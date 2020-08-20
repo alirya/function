a67a6cbdc9dda96c4a6402eb148c1ea009d14cda
dikac <rigel234@yahoo.com>
2020-08-20 12:13:53 +0700

    make 'assert/assert.ts' Extra parameter template to be optional

--- dist/assert/assert.d.ts
+++ dist/assert/assert.d.ts
@@ -1,2 +1,2 @@
-declare type Assert<Argument, Return extends Argument, Extras extends unknown[]> = (argument: Argument, ...extras: Extras) => asserts argument is Return;
+declare type Assert<Argument, Return extends Argument, Extras extends unknown[] = unknown[]> = (argument: Argument, ...extras: Extras) => asserts argument is Return;
 export default Assert;
--- src/assert/assert.ts
+++ src/assert/assert.ts
@@ -1,3 +1,3 @@
 
-type Assert<Argument, Return extends Argument, Extras extends unknown[]> = (argument : Argument, ...extras : Extras) => asserts argument is Return
+type Assert<Argument, Return extends Argument, Extras extends unknown[] = unknown[]> = (argument : Argument, ...extras : Extras) => asserts argument is Return
 export default Assert;
