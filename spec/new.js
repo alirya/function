d4337704d2814a776edb23d12960898282852ec7
dikac <rigel234@yahoo.com>
2022-01-24 21:40:17 +0700

    add debug spec config

---
+++ spec/new.js
@@ -0,0 +1,35 @@
+import New from '../dist/new';
+it('force console log', () => { spyOn(console, 'log').and.callThrough(); });
+describe('array', function () {
+    let fn = New(Array);
+    it('compiler compatible', () => {
+        fn(1, 2, 3, 4);
+        // @ts-expect-error
+        fn('1', '2', '3', '4');
+    });
+    it('validate result', () => {
+        expect(fn(1, 2, 3, 4)).toEqual([1, 2, 3, 4]);
+    });
+});
+describe('user defined', function () {
+    class R {
+        constructor(a, b, c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    }
+    let fn = New(R);
+    it('compiler compatible', () => {
+        let result = fn('1', 2, true);
+        // @ts-expect-error
+        fn('1', '2', '3', '4');
+    });
+    it('validate result', () => {
+        let result = fn('1', 2, true);
+        expect(result.a).toEqual('1');
+        expect(result.b).toEqual(2);
+        expect(result.c).toEqual(true);
+    });
+});
+//# sourceMappingURL=new.js.map
--- src/new.ts
+++ src/new.ts
@@ -14,4 +14,3 @@ export default function New<
         return new constructor(...argument);
     };
 }
-
