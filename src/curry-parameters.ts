aa46763f1d01e2e2223e38d0650d4085ae2eebcf
dikac <rigel234@yahoo.com>
2022-02-28 23:26:16 +0700

    fix argument to any

--- src/curry-parameters.ts
+++ src/curry-parameters.ts
@@ -1,7 +1,7 @@
 import Callable from './callable';
 import Heads from '@alirya/array/heads';
 import Tails from '@alirya/array/tails';
-import InsertArray from '@alirya/array/function/insert-parameters';
+import InsertArray from '@alirya/array/insert-parameters';
 
 export type CurryTypeArgument<Array extends unknown[], Position extends number> = [...Heads<Array, Position>, ...Tails<Array, Position>];
 
@@ -9,7 +9,7 @@ export default function CurryParameters<
     Callback extends Callable,
     Arguments extends Parameters<Callback> = Parameters<Callback>,
     Position extends number = number
-    >(
+>(
     callback : Callback,
     argument : Arguments[Position],
     position : Position
@@ -17,8 +17,8 @@ export default function CurryParameters<
 
     return function (...args) {
 
-        return callback(...InsertArray(args, position)(argument));
+        return callback(...InsertArray(args, argument as any, position));
 
-    } as Callable<CurryTypeArgument<Arguments, Position>, ReturnType<Callback>> ;
+    } as Callable<CurryTypeArgument<Arguments, Position>, ReturnType<Callback>>;
 }
 
